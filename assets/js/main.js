638c400c31846aa5be40c7aeaf09184f47102111
John Nutter <john@terribledevice.com>
2020-07-25 05:42:28 -0400

    added some javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
-$("div input").hover(function() {
-    $(this).addClass("blue");
-  }, function() {
-    $(this).removeClass("blue");
-  });
+function changeText(text)
+{
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = "H̶͉̓ị̵̡͂,̶̯̄ ̷̥̎Ĩ̴͍̤̅'̸̙͋ṁ̴̼̠ ̵̠̪̾̀J̵̱̄ȍ̵̗̹̌h̸̰͎̔n̷͇̍ͅ ̴̼̻̄Ṅ̵̹̣̈u̴̬̓t̶̨̧͋t̸͇́́ẹ̷̡͌̈́r̵̺͆!̶̣̎";
+    document.getElementById("text-display").style.color = "#0ff";
+}
+      
+function defaultText()
+{
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = "Hi, I'm John Nutter!";
+    document.getElementById("text-display").style.color = "#F294EC";
+}
