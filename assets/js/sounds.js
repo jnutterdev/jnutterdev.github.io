78608753ac162589bd05f9bbde6a1e4d1fb5bd11
terribledevice <longlost@gmail.com>
2020-11-03 16:09:43 -0500

    added some more sounds

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -1,37 +1,12 @@
-const player = new Tone.Player("../assets/sounds/8bit2.mp3").toMaster();
-// const stopButton = document.getElementById("stop-button");
-// const toneMeter = new Tone.Meter({
-//     channels: 1,
-// });
-let start;
+const player = new Tone.Player("../assets/sounds/8bit3.wav").toMaster();
 const element = document.getElementById("content");
 
 
-function step(timestamp) {
-    if (start === undefined)
-        start = timestamp;
-    const elapsed = timestamp - start;
-
-    // `Math.min()` is used here to make sure that the element stops at exactly 200px.
-    element.style.transform = 'translateX(' + Math.min(0.1 * elapsed, 200) + 'px)';
-
-    if (elapsed < 10000) { // Stop the animation after 2 seconds
-        window.requestAnimationFrame(step);
-    }
-
-}
-
 
 Tone.loaded().then(() => {
+
     player.loop = true;
-    // player.connect(toneMeter);
     // player.start(0.1);
 
-    // window.requestAnimationFrame(step);
-
-    // meter({
-    //     tone: toneMeter,
-    //     parent: document.querySelector("#content")
-    // });
 
 });
