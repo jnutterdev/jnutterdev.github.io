6a05fd1c6de8deb9932b94cf0f44eb1420fea011
John Nutter <john@terribledevice.com>
2020-11-05 15:02:11 -0500

    added music player controls. updated tone.js. worked on layout a little more

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -1,12 +1,21 @@
-const player = new Tone.Player("../assets/sounds/8bit3.wav").toMaster();
-const element = document.getElementById("content");
-
+const player = new Tone.Player("../assets/sounds/8bit3.wav").toDestination();
+const startButton = document.getElementById("start-button");
+const stopButton = document.getElementById("stop-button");
 
 
 Tone.loaded().then(() => {
 
     player.loop = true;
-    // player.start(0.1);
+    player.start(0.1);
 
+    stopButton.addEventListener('click', () => {
+        player.stop();
+    })
+
+    startButton.addEventListener('click', () => {
+        player.loop = true;
+        player.start(0.1);
+
+    })
 
 });
